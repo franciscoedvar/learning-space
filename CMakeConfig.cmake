0651861629d5fead1f5db9a2abe954b3fcd8f4e2
Francisco Edvar <edvar.cunha@gmail.com>
2024-08-12 02:43:19 -0300

    Created a cmakeconfig file to save all necessary configs

---
+++ CMakeConfig.cmake
@@ -0,0 +1,13 @@
+# Configurations for config.h.in
+
+## 1.DATA ##
+# Data Directory
+set(DATA_DIR "${CMAKE_CURRENT_SOURCE_DIR}/data")
+
+# Digits Processed - Extraction Window 
+# Set true to process data for each square window NxN
+# 1 for True and 0 for False
+set(EXTRACT_W_OP_DIGITS_3_3 1)
+set(EXTRACT_W_OP_DIGITS_5_5 1)
+set(EXTRACT_W_OP_DIGITS_7_7 1)
+
